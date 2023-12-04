7cb206e5b6cf0b843527fa5f2e1c0b49d13b9ef3
Tuana <57767763+tuananohut@users.noreply.github.com>
2023-12-04 19:17:41 +0300

    Add files via upload

---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,54 @@
+using System.Globalization;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    [SerializeField]
+    Transform pointPrefab;
+
+    [SerializeField, Range(10, 100)]
+    int resolution = 10;
+
+    [SerializeField]
+    FunctionLibrary.FunctionName function;
+
+    Transform[] points;
+
+    void Awake()
+    {
+        float step = 2f / resolution;
+        var position = Vector3.zero;
+        var scale = Vector3.one * step;
+        points = new Transform[resolution * resolution];
+        
+        for(int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
+        {
+            if(x == resolution) 
+            { 
+                x = 0;
+                z += 1;
+            }
+
+            Transform point = points[i] = Instantiate(pointPrefab);
+            position.x = (x + 0.5f) * step - 1f;
+            position.z = (z + 0.5f) * step - 1f;
+            point.SetParent(transform, false); 
+            point.localPosition = position;
+            point.localScale = scale;
+        }
+    }
+
+    void Update()
+    {
+        FunctionLibrary.Function f = FunctionLibrary.GetFunction(function); 
+        float time = Time.time;
+        for(int i = 0; i < points.Length; i++) 
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = f(position.x, position.z,  time);
+            point.localPosition = position;
+        }
+    }
+
+}
